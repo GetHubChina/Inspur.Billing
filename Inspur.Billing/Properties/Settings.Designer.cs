749a5eb5e2063a7f5a0feafdeece0c3c36979409
fengjuanchensha <1130217572@qq.com>
2018-04-12 18:30:39 +0800

    20180412
    20180412

--- Inspur.Billing/Properties/Settings.Designer.cs
+++ Inspur.Billing/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Inspur.Billing.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace Inspur.Billing.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=D:\\开票软件\\开票软件\\Inspur.Billing - 副本\\Inspur.Billing\\bin\\Debug\\Billing.db")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=D:\\GitHub\\Inspur.Billing\\Inspur.Billing\\bin\\Debug\\Billing.db")]
         public string connectionString {
             get {
                 return ((string)(this["connectionString"]));
