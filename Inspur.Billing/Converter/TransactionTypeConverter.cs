2bf1fa7c4d52b555056f5c029087422724db1549
GetHubChina <364154675@qq.com>
2018-10-30 18:24:25 +0800

    退票修改
    退票过的，退票按钮置灰；添加退票成功的提示信息

---
+++ Inspur.Billing/Converter/TransactionTypeConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inspur.Billing.Converter
+{
+    public class TransactionTypeConverter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = false;
+            if (value != null)
+            {
+                switch (value.ToString())
+                {
+                    case "0":
+                        result = true;
+                        break;
+                    case "1":
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
